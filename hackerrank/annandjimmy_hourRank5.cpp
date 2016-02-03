6001972015aeb808989eaf781f087f92712c7aa3
gitmenonsandu <sandeep.n.menon@ieee.org>
2016-02-04 00:25:05 +0530

    HourRank5

---
+++ hackerrank/annandjimmy_hourRank5.cpp
@@ -0,0 +1,37 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    // your code goes here
+    int a=n/3;
+    n-=a;
+    int b=n/2;
+    n-=b;
+    printf("%d",a*b*n);
+    return 0;
+}
