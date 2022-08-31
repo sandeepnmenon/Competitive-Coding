56666bd395f9f933933646ac039f0ce1734f0173
Sandeep Menon <sandeepmenon@deepen.ai>
2022-08-31 00:21:46 -0400

    Word Search II completed

--- leetcode/word-search-ii.cpp
+++ leetcode/word-search-ii.cpp
@@ -1,27 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-class Trie {
+class Trie
+{
 private:
-    Trie* edges[26] = {};
+    Trie *edges[26] = {};
     bool isWordEnd = false;
+
 public:
     string word;
-    Trie() {
+    Trie()
+    {
     }
     ~Trie()
     {
-        for(int i=0;i<26;++i)
+        for (int i = 0; i < 26; ++i)
             delete this->edges[i];
     }
-    void insert(string word) {
-        Trie* traverse = this;
+    void insert(string word)
+    {
+        Trie *traverse = this;
         int len = word.length();
-        for(int i = 0; i < len; ++i)
+        for (int i = 0; i < len; ++i)
         {
             int ch = word[i] - 'a';
-            if(!traverse->isEdge(word[i]))
+            if (!traverse->isEdge(word[i]))
             {
                 traverse->edges[ch] = new Trie();
             }
@@ -36,7 +39,7 @@ public:
         return this->edges[ch - 'a'] != NULL;
     }
 
-    Trie* traverseEdge(char ch)
+    Trie *traverseEdge(char ch)
     {
         return this->edges[ch - 'a'];
     }
@@ -47,66 +50,70 @@ public:
     }
 };
 
-class Solution {
+class Solution
+{
 public:
-    void findInTrie(int i, int j, vector<vector<char>>& board, Trie* root, int m, int n, unordered_set<string> &ans)
+    void findInTrie(int i, int j, vector<vector<char>> &board, Trie *root, int m, int n,
+                    vector<vector<bool>> &visited, unordered_set<string> &ans)
     {
-        if(root->isWordEnding())
-        {
-            ans.insert(root->word);
-        }
-
-        if(i<0 or j<0 or i==m or j==n)
+        if (i < 0 or j < 0 or i == m or j == n or visited[i][j])
             return;
-        
-        if(!root->isEdge(board[i][j]))
+
+        if (!root->isEdge(board[i][j]))
         {
             return;
         }
 
         root = root->traverseEdge(board[i][j]);
+        visited[i][j] = true;
+        if (root->isWordEnding())
+        {
+            ans.insert(root->word);
+        }
 
-        findInTrie(i - 1, j, board, root, m, n, ans);
-        findInTrie(i, j - 1, board, root, m, n, ans);
-        findInTrie(i + 1, j, board, root, m, n, ans);
-        findInTrie(i, j + 1, board, root, m, n, ans);
+        findInTrie(i - 1, j, board, root, m, n, visited, ans);
+        findInTrie(i, j - 1, board, root, m, n, visited, ans);
+        findInTrie(i + 1, j, board, root, m, n, visited, ans);
+        findInTrie(i, j + 1, board, root, m, n, visited, ans);
+        visited[i][j] = false;
     }
 
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Trie* root = new Trie();
-        for(string word: words)
+    vector<string> findWords(vector<vector<char>> &board, vector<string> &words)
+    {
+        Trie *root = new Trie();
+        for (string word : words)
         {
             root->insert(word);
         }
         int m = board.size(), n = board[0].size();
         vector<string> ans;
         unordered_set<string> temp_ans;
-        for(int i = 0; i < m ; ++i)
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        for (int i = 0; i < m; ++i)
         {
-            for(int j = 0; j < n; ++j)
+            for (int j = 0; j < n; ++j)
             {
-                findInTrie(i, j, board, root, m, n, temp_ans);
+                findInTrie(i, j, board, root, m, n, visited, temp_ans);
             }
         }
-        
+
         ans.insert(ans.end(), temp_ans.begin(), temp_ans.end());
 
         return ans;
     }
 };
 
-
 int main()
 {
-    vector<vector<char>> board = {{'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'}};
-    vector<string> words = {"oath","pea","eat","rain"};
+    vector<vector<char>> board = {{'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}};
+    vector<string> words = {"oath", "pea", "eat", "rain"};
     Solution sol;
     vector<string> ans = sol.findWords(board, words);
-    for(string str: ans)
+    for (string str : ans)
     {
-        cout<<str<<" ";
+        cout << str << " ";
     }
-    cout<<endl;
+    cout << endl;
 
     return 0;
 }
